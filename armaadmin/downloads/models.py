ccf1f87a0f647a91a20c6d1634e38109e5cf19b3
Sam Collins <sam@scollins.co>
2017-04-16 15:37:41 +0100

    Created File Model

--- armaadmin/downloads/models.py
+++ armaadmin/downloads/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.timezone import now
+
+
+class File(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    filename = models.CharField(max_length=255)
+    path = models.CharField(max_length=255)
+    hash = models.CharField(max_length=32)
+    download = models.URLField()
+    created = models.DateTimeField(default=now, editable=False)
